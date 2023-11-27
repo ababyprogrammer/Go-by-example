2931e80214f3a0dd2815b007e9f8907b6d11a096
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2023-11-27 12:04:38 +0330

    Add files via upload

---
+++ Go/hello-world.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world!")
+}
+
+/** For run in the Terminal Window type:
+
+$ go run hello-world.go
+hello, world!
+
+$ go build hello-world.go
+$ ls
+hello-world 	hello-world.go
+
+$ ./hello-world
+hello, world
+*/
